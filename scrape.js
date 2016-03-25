71d87b36f5055d2e1f2f072ba29077fc59c4c12a
trevor millner <trevormillner@yahoo.com>
2016-03-24 19:45:22 -0700

    task - Extend busted functionality -- mocking

--- scrape.js
+++ scrape.js
@@ -16,12 +16,45 @@ var URL = "https://en.wikipedia.org/wiki/Lists_of_cities_by_country";
 xray(URL, 'ul li b a:not([class])', [{
     country: '',
     _link: 'a@href' /* innerText is better than title (which lies) */
-}])(function(err, customResponse) {
-  var transformOperation = customResponse.map(function(item) {
+}])(function(err, countryResponse) {
+  var transformCountryOperation = countryResponse.map(function(item) {
     var country = item.country.split("in ")[1];
     return {country: country};
   });
-  fs.writeFile('countries.json', JSON.stringify(transformOperation, null, " "));
+  fs.writeFile('countries.json', JSON.stringify(transformCountryOperation, null, " "));
+
+  // handle_name_city_links
+  xray(countryResponse._link, "table.wikitable.sortable.jquery-tablesorter thead tr th", [{
+    heading: ''
+  }])(function(err, cityResponse) {
+    console.log("INSIDE!");
+    if (err) {
+      console.log("Oh snap! It failed " + err);
+    }
+    cityResponse.findIndex(function(element, index, array) {
+      if (element.toLowerCase().search("city") !== -1 ||
+        element.toLowerCase().search("cities") !== -1 ||
+        element.toLowerCase().search("name") !== -1 ||
+        element.toLowerCase().search("community") !== -1 ||
+        element.toLowerCase().search("english") !== -1) {
+        /* TODO Error checking (index === -1) ! */
+        xray(countryResponse.link, 
+          "table.wikitable.sortable.jquery-tablesorter tbody tr td:nth-child(" + index + ")", [{
+            city: '',
+            _link: 'a@href'
+          }])(function(err, specificCityResponse) {
+            if (err) {
+              console.log("Another failure!!");
+            }
+            fs.writeFile('cities.json', JSON.stringify(specificCityResponse, null, " "));
+          })
+      } 
+    })
+
+
+    // Go to this page again with the links of cities now found
+    xray()
+  })
 });
 
 /*
